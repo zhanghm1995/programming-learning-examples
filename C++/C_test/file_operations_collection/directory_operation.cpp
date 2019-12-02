188144e9b10bc3dffe4a4eb67720acb9e1b0f118
zhanghming <zhanghm_1995@qq.com>
2019-12-02 23:13:25 +0800

    add file_operations_collection codes

---
+++ C++/C_test/file_operations_collection/directory_operation.cpp
@@ -0,0 +1,82 @@
+/*======================================================================
+* Author   : Haiming Zhang
+* Email    : zhanghm_1995@qq.com
+* Version  :　2019年12月2日
+* Copyright    :
+* Descriptoin  : Learn how to use C++ to process directory and files related demands
+* References   :
+======================================================================*/
+
+#include <dirent.h>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/**
+ * @brief Assume no subdirectory in dir_name folder
+ * 
+ **/  
+void ListFilesInDirectory(const std::string& dir_name)
+{
+    DIR* dir;
+    struct dirent* ent;
+
+    int total_files = 0;
+    if ((dir = opendir(dir_name.c_str())) != NULL) {
+        // print all the files and directories within directory
+        while ((ent = readdir(dir)) != NULL) {
+            printf("%s\n", ent->d_name);
+
+            // skip . & ..
+            if (strlen(ent->d_name) > 2) {
+                ++total_files;
+            }
+        }
+        closedir(dir);
+        printf("Total %d files in %s\n", total_files, dir_name.c_str());
+    } else {
+        // cannot open the directory
+        perror("Failed to open directory");
+        return;
+    }
+}
+
+void CountImageNumInDirectory(const std::string& dir_name)
+{
+    const std::vector<std::string> exts = {".jpg", ".jpeg", ".png", ".bmp", ".JPG", ".JPEG", ".PNG", ".BMP"};
+
+     DIR* dir;
+    struct dirent* ent;
+
+    int total_num = 0;
+    if ((dir = opendir(dir_name.c_str())) != NULL) {
+        // print all the files and directories within directory
+        while ((ent = readdir(dir)) != NULL) {
+            std::string name(ent->d_name);
+            size_t lDotPos = name.find_last_of(".");
+            
+            if (lDotPos != std::string::npos) {
+                std::string ext = name.substr(lDotPos);
+                if (std::find(exts.begin(), exts.end(), ext) != exts.end()) {
+                    ++total_num;
+                    printf("%s\n", name.c_str());
+                }
+            }
+        }
+        closedir(dir);
+        printf("\nTotal %d images in %s\n", total_num, dir_name.c_str());
+    } else {
+        // cannot open the directory
+        perror("Failed to open directory");
+        return;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // ListFilesInDirectory("/home/zhanghm/Test");
+    CountImageNumInDirectory("/home/zhanghm/Test");
+
+    return 0;
+}
