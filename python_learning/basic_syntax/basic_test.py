8083b04173444193507fa029aa9c636bb75a495a
zhanghming <zhanghm_1995@qq.com>
2019-12-09 23:07:41 +0800

    add class_test.py

--- python_learning/basic_syntax/basic_test.py
+++ python_learning/basic_syntax/basic_test.py
@@ -1,11 +1,38 @@
 import keyword
-
+import math
 
 # print(keyword.kwlist)
 
+def reverseWords(input):
+     
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
+ 
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],  
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    print(inputWords, len(inputWords))
+    inputWords=inputWords[-1::-1]
+    print(inputWords)
+ 
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+     
+    return output
 
 if __name__ == "__main__":
     print("hello world")
 
-    str = 'zhanghaiming'
-    print(str[2:5])
+    input = "I like you"
+    print(max(input))
+
+    var1 = 10
+    print(var1)
+    del var1
+    
+
+    
