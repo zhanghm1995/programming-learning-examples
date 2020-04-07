5d879feff1e0f178cac6ffc374437104dcb7e0a2
zhanghming-desktop <zhanghm_1995@qq.com>
2020-04-07 21:42:39 +0800

    [C++] update macro_test.cpp

--- C++/C_test/macro_test/macro_test.cpp
+++ C++/C_test/macro_test/macro_test.cpp
@@ -4,9 +4,19 @@
 #include "macro_define.h"
 
 using namespace std;
+
+/// Define some useful printf macro
+#define AINFO(format, ...) printf("[INFO] " format "\n", ##__VA_ARGS__)
+#define AWARN(format, ...) printf("[WARN]" format "\n", ##__VA_ARGS__)
+
 int main()
 {
     Example1 example1;
     cout<<example1.GetLength()<<endl;
     cout<<MaxLength<<endl;
+
+    cout<<"================="<<endl;
+    AINFO("This is number %d", 10);
+    AWARN("Failed to load %s", "arguments");
+    return 0;
 }
