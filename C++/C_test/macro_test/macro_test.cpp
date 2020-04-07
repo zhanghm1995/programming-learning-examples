d0299a9e8ac7c4f370a03c76a6d28cc20cf70db4
zhanghming-desktop <zhanghm_1995@qq.com>
2020-04-07 21:51:56 +0800

    [C++] add PrintInfoWithColor function in macro_test.cpp

--- C++/C_test/macro_test/macro_test.cpp
+++ C++/C_test/macro_test/macro_test.cpp
@@ -7,7 +7,14 @@ using namespace std;
 
 /// Define some useful printf macro
 #define AINFO(format, ...) printf("[INFO] " format "\n", ##__VA_ARGS__)
-#define AWARN(format, ...) printf("[WARN]" format "\n", ##__VA_ARGS__)
+#define AWARN(format, ...) printf("\033[1;33m[WARN] " format "\033[0m\n", ##__VA_ARGS__)
+#define AERROR(format, ...) printf("\033[1;31m[ERROR] " format "\033[0m\n", ##__VA_ARGS__)
+void PrintInfoWithColor() {
+    printf("\033[1;33mHello, world!\033[0m\n"); // yellow
+
+    int count = 10;
+    printf("\033[1;31m[INFO] There are %d images for processing...\033[0m\n", count);
+}
 
 int main()
 {
@@ -16,7 +23,11 @@ int main()
     cout<<MaxLength<<endl;
 
     cout<<"================="<<endl;
+    PrintInfoWithColor();
+
+    cout<<"================="<<endl;
     AINFO("This is number %d", 10);
-    AWARN("Failed to load %s", "arguments");
+    AWARN("Delta time is less than %d", 1);
+    AERROR("Failded to init!");
     return 0;
 }
