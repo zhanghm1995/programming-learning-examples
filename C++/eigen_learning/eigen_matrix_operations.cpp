a6b1f50e5fb659610e27ceafca4e46a33912a5ff
zhanghming-desktop <zhanghm_1995@qq.com>
2020-04-12 11:33:27 +0800

    [C++] update eigen_learning

--- C++/eigen_learning/eigen_matrix_operations.cpp
+++ C++/eigen_learning/eigen_matrix_operations.cpp
@@ -2,7 +2,7 @@
  * @Author: Haiming Zhang
  * @Email: zhanghm_1995@qq.com
  * @Date: 2020-04-11 08:34:02
- * @LastEditTime: 2020-04-11 14:27:53
+ * @LastEditTime: 2020-04-11 16:20:25
  * @References: 
  * @Description: 
  */
@@ -16,6 +16,7 @@ using std::cout, std::endl;
 /**
  * @brief Using << to stack two matrix, note that the VectorXf is column vector by default
  *                Eigen choose to stack two matrix horizontally or vertically according to the output matrix size
+ * @ref https://stackoverflow.com/questions/21496157/eigen-how-to-concatenate-matrix-along-a-specific-dimension
  */ 
 void StackTwoMatrix() {
   // Eigen::MatrixXf A(3,  4);
@@ -59,11 +60,41 @@ Eigen::MatrixXf ReshapeMatrix() {
   return M3;
 }
 
+void OperateMatrixRowsAndCols() {
+  /// 1) 矩阵每一行除以最后一行
+  Eigen::MatrixXf M1(3, 3); // Column major storage
+  M1 << 4, 5, 6,
+               7, 8, 9,
+               1, 2, 3;
+  M1.array().rowwise() /= M1.row(2).array();
+  cout<<M1.topRows(2)<<endl;
+}
+
+/**
+ * @brief
+ * @ref https://eigen.tuxfamily.org/dox/group__TutorialBlockOperations.html
+ */ 
+void OperateBlock() {
+  /// 1) 将矩阵转为齐次矩阵
+  Eigen::Matrix4f temp2 = Eigen::Matrix4f::Zero();
+  temp2(3, 3) = 1.0;
+  Eigen::MatrixXf M1(3, 3); // Column major storage
+  M1 << 4, 5, 6,
+               7, 8, 9,
+               1, 2, 3;
+  temp2.topLeftCorner(3, 3) = M1;
+  cout<<temp2<<endl;
+}
+
 int main() {
   cout<<"====================="<<endl;
   StackTwoMatrix();
   cout<<"====================="<<endl;
   Eigen::MatrixXf matrix = ReshapeMatrix();
   cout<<"matrix = "<<matrix<<endl;
+  cout<<"====================="<<endl;
+  OperateMatrixRowsAndCols();
+  cout<<"====================="<<endl;
+  OperateBlock();
   return 0;
 }
