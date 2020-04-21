bd72cf7bb50a6ebb5530bd594d579cba85c9eeba
zhanghming-desktop <zhanghm_1995@qq.com>
2020-04-21 22:32:51 +0800

    [C++] add BasicDataType function in eigen_basic.cpp

--- C++/eigen_learning/eigen_basic.cpp
+++ C++/eigen_learning/eigen_basic.cpp
@@ -2,7 +2,7 @@
  * @Author: Haiming Zhang
  * @Email: zhanghm_1995@qq.com
  * @Date: 2020-04-21 21:33:06
- * @LastEditTime: 2020-04-21 21:55:06
+ * @LastEditTime: 2020-04-21 22:32:06
  * @References: 
  * @Description: Learn Eigen basic usages
  */
@@ -13,6 +13,32 @@
 using std::cout;
 using std::endl;
 
+
+void BasicDataType() {
+  /// Default vector is a column vector, use Eigen::RowVectorXf if you want row vector
+  Eigen::VectorXf v1(3);
+  v1<<1,2,3;
+  cout<<"v1=\n"<<v1<<endl;
+  // typedef Eigen::Matrix<float, 1, -1> Eigen::RowVectorXf
+  Eigen::RowVectorXf v2(3);
+  v2<<1,2,3;
+  cout<<"v2=\n"<<v2<<endl;
+
+  /// Eigen matrix is stored in Column-Major
+  Eigen::MatrixXf M1(3, 3); // Column major storage
+  M1 << 1, 2, 3,
+               4, 5, 6,
+               7, 8, 9;
+  // Convert Column-major storage matrix to Row-major storage matrix
+  Eigen::Matrix<float, -1, -1, Eigen::RowMajor> M2(M1);
+  cout<<"M2=\n"<<M2<<endl;
+}
+
+/**
+ * @brief Learn the matrix and vector initialization
+ * @ref https://blog.csdn.net/zhanghm1995/article/details/80709455
+ *            https://eigen.tuxfamily.org/dox/group__TutorialAdvancedInitialization.html
+ */ 
 void CreateAndInitializeMatrix() {
   /// Default initialization
   Eigen::Matrix3f a; // random value
@@ -37,6 +63,7 @@ void CreateAndInitializeMatrix() {
   m1 <<1,2,3,
               4,5,6,
               7,8,9;
+
   /// Special matrix initialization
   Eigen::MatrixXf M1 = Eigen::MatrixXf::Zero(3,4);
   Eigen::MatrixXf M2 = Eigen::MatrixXf::Ones(3,3);
@@ -47,6 +74,9 @@ void CreateAndInitializeMatrix() {
 }
 
 int main() {
+  cout<<"=============CreateAndInitializeMatrix================="<<endl;
   CreateAndInitializeMatrix();
+  cout<<"=============BasicDataType================="<<endl;
+  BasicDataType();
   return 0;
 }
