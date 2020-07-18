00c5c46c561bb81104eaaaabb20049d6972ac039
zhanghming-desktop <zhanghm_1995@qq.com>
2020-07-18 09:35:21 +0800

    [C++] update sth

--- C++/C_test/numerical_calculation.cpp
+++ C++/C_test/numerical_calculation.cpp
@@ -30,15 +30,25 @@ void TestNaN() {
 }
 
 void TestNumericalLimit() {
+    int int_min = std::numeric_limits<int>::min();
+    cout<<"int_min: "<<int_min<<endl;
+    int int_max = std::numeric_limits<int>::max();
+    cout<<"int_max: "<<int_max<<endl;
+
+    unsigned int unsigned_int_min = std::numeric_limits<unsigned int>::min();
+    cout<<"unsigned_int_min: "<<unsigned_int_min<<endl;
+    unsigned int unsigned_int_max = std::numeric_limits<unsigned int>::max();
+    cout<<"unsigned_int_max: "<<unsigned_int_max<<endl;
+
     float float_min = std::numeric_limits<float>::min();
-    cout<<float_min<<endl;
-     float float_max = std::numeric_limits<float>::max();
-    cout<<float_max<<endl;
+    cout<<"float_min: "<<float_min<<endl;
+    float float_max = std::numeric_limits<float>::max();
+    cout<<"float_max: "<<float_max<<endl;
 
     double double_min = std::numeric_limits<double>::min();
-    cout<<double_min<<endl;
+    cout<<"double_min: "<<double_min<<endl;
     double double_max = std::numeric_limits<double>::max();
-    cout<<double_max<<endl;
+    cout<<"double_max: "<<double_max<<endl;
 
     double_min = DBL_MIN;
     cout<<double_min<<endl;
