5ea363554210defa0201d709ae2b0e41a2ef8248
zhanghming-desktop <zhanghm_1995@qq.com>
2020-07-25 21:48:26 +0800

    [C++] add c++11_test.cpp

---
+++ C++/C_test/c++11_test.cpp
@@ -0,0 +1,37 @@
+// https://stackoverflow.com/questions/16455029/how-to-implement-an-initializer-list-for-user-defined-type-analogus-to-stdve
+
+#include <vector>
+#include <iostream>
+#include <initializer_list>
+
+using std::cout;
+using std::endl;
+
+/**
+ * Learn how to use std::initializer_list
+ * https://stackoverflow.com/questions/16455029/how-to-implement-an-initializer-list-for-user-defined-type-analogus-to-stdve
+ */
+template <typename T>
+class foo {
+public:
+    foo(std::initializer_list<T> init) 
+      : vec(init)
+    { }
+
+    void print() {
+        for (const auto& v : vec) {
+            cout<<v<<endl;
+        }
+    }
+
+private:
+    std::vector<T> vec;
+};
+
+int main()
+{
+    foo<int> f = {1, 2, 3, 4, 5};
+    f.print();
+
+    return 0;
+}
