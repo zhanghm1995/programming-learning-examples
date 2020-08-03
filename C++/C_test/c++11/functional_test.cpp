ba754111035a99cb5004a1c517a4182e403f1cd4
zhanghming-desktop <zhanghm_1995@qq.com>
2020-08-03 22:48:21 +0800

    [C++] add c++ 11 testing codes

---
+++ C++/C_test/c++11/functional_test.cpp
@@ -0,0 +1,72 @@
+//代码出自链接：http://www.jellythink.com/archives/771
+// 
+
+#include <functional>
+#include <iostream>
+using namespace std;
+
+std::function< int(int)> Functional;
+
+// 普通函数
+int TestFunc(int a)
+{
+    return a;
+}
+
+// Lambda表达式
+auto lambda = [](int a)->int{ return a; };
+
+// 仿函数(functor)
+class Functor
+{
+public:
+    int operator()(int a)
+    {
+        return a;
+    }
+};
+
+// 1.类成员函数
+// 2.类静态函数
+class TestClass
+{
+public:
+    int ClassMember(int a) { return a; }
+    static int StaticMember(int a) { return a; }
+};
+
+int main()
+{
+    //////////////////////学习使用std::function简化调用流程//////////////////
+    // Reference: https://blog.csdn.net/wangshubo1989/article/details/49134235#comments
+    // https://blog.csdn.net/smstong/article/details/44958833
+    // 普通函数
+    Functional = TestFunc;
+    int result = Functional(10);
+    cout << "普通函数："<< result << endl;
+
+    // Lambda表达式
+    Functional = lambda;
+    result = Functional(20);
+    cout << "Lambda表达式："<< result << endl;
+
+    // 仿函数
+    Functor testFunctor;
+    Functional = testFunctor;
+    result = Functional(30);
+    cout << "仿函数："<< result << endl;
+
+    // 类成员函数
+    TestClass testObj;
+    Functional = std::bind(&TestClass::ClassMember, testObj, std::placeholders::_1);
+    result = Functional(40);
+    cout << "类成员函数："<< result << endl;
+
+    // 类静态函数
+    Functional = TestClass::StaticMember;
+    result = Functional(50);
+    cout << "类静态函数："<< result << endl;
+    //////////////////////学习使用std::function简化调用流程//////////////////
+
+    return 0;
+}
