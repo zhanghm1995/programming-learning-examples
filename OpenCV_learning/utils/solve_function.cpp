83678da4506bb1989bc000b12dba41fa55cf5f15
zhanghming-desktop <zhanghm_1995@qq.com>
2020-08-06 23:34:16 +0800

    [OpenCV_Learning] add solve_function.cpp

---
+++ OpenCV_learning/utils/solve_function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+
+double Divide() {
+    double a = 0.0;
+    double b = 0.0;
+    return a / b;
+}
+
+int main() {
+    std::vector<double> coefs = {1, 0, 0};
+    std::vector<double> coefs1 = {1, 5, 4};
+    std::vector<double> coefs2 = {0, 0, 4};
+    std::vector<double> coefs3 = {0, 0, 0};
+    std::vector<double> roots;
+    cv::Mat rots;
+    auto ret = cv::solvePoly(coefs3, rots);
+    cout<<rots.type()<<" "<<CV_64FC1<<endl;
+    cout<<ret<<endl;
+
+    cout<<rots<<endl;
+
+    auto ret2 = Divide();
+    auto ret3 = NAN;
+    cout<<ret2<<endl;
+    return 0;
+}
