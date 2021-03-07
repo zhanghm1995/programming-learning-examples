7ab2ec7f50faab85e0b9bcb572db9c0da966f647
zhanghming-desktop <zhanghm_1995@qq.com>
2021-03-07 18:22:09 +0800

    [Python_learning] add data folder to retain the resources

--- Python_learning/read_file.py
+++ Python_learning/read_file.py
@@ -1,7 +1,15 @@
 import os
+import numpy as np
 
-def read_kitti_oxts(self, dataset_root, sequence_num):
-    oxts_file = os.path.join(dataset_root, 'oxts/{0:04d}'.format(int(sequence_num)))
+def read_txt_to_numpy(file_path):
+    a = np.loadtxt(file_path)
+    print(a.shape, a.dtype)
+
+def read_kitti_oxts(dataset_root, sequence_num):
+    """
+    read txt file line by line to a dictionary
+    """
+    oxts_file = os.path.join(dataset_root, 'oxts/{0:04d}.txt'.format(int(sequence_num)))
 
     with open(oxts_file, 'r') as f:
         lines = f.readlines()
@@ -12,12 +20,9 @@ def read_kitti_oxts(self, dataset_root, sequence_num):
         oxts_dict[i] = oxt
     return oxts_dict  
 
-with open("/mnt/study/Datasets/KITTI/tracking/training/oxts/0008.txt") as f:
-    lines = f.readlines()
+if __name__ == '__main__':
+    oxts_dict = read_kitti_oxts("data", 8)
+    print(type(oxts_dict))
 
-oxts_dict = {}
-for i, line in enumerate(lines):
-    oxt = line.strip().split(' ')
-    oxts_dict[i] = oxt
+    read_txt_to_numpy("data/oxts/0008.txt")
     
-print(oxts_dict)
