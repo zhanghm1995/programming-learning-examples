3e4168b08067dc19623985a5a94996ca0b1130a0
zhanghming <zhanghm_1995@qq.com>
2022-02-25 14:40:28 +0800

    update Python_learning

--- Python_learning/basics/class_test.py
+++ Python_learning/basics/class_test.py
@@ -2,26 +2,27 @@
 ref: https://www.runoob.com/python3/python3-class.html
 """
 
-class MyClass:
-    """一个简单的类实例"""
+from turtle import forward
 
-    def __init__(self):
-        self.data = [1,2,3]
-        self.i = 23
 
-    i = 12345
-    __weight = 12 # private member
-    def hello_world(self):
-        print(self.__weight)
-        return 'hello world'
+class BaseExample:
+    def __init__(self) -> None:
+        pass
 
-    def __len__(self):
-        return len(self.data)
+    def forward(self):
+        print("This is in BaseExample class")
+    
 
-# 实例化类
-x = MyClass()
- 
-# 访问类的属性和方法
-print("MyClass 类的属性 data 为：", len(x))
-print("MyClass 类的属性 i 为：", x.i)
-print("MyClass 类的方法 f 输出为：", x.hello_world())
+class DerivedExample(BaseExample):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self):
+        super().forward()
+        print("This is in DerivedExample class")
+    
+
+if __name__ == "__main__":
+    derived_example = DerivedExample()
+
+    derived_example.forward()
