5c37052a3e58830086be74f423416cf7264ba7ef
Herman <zhanghm_1995@qq.com>
2022-07-14 22:22:53 +0800

    update stat_bad_data.py

--- Python_learning/filesystem_utils/stat_bad_data.py
+++ Python_learning/filesystem_utils/stat_bad_data.py
@@ -13,29 +13,43 @@ from glob import glob
 from tqdm import tqdm
 
 
+
 def get_folder_list(root_dir):
     folder_list = [entry.path for entry in os.scandir(root_dir) if entry.is_dir()]
     folder_list = sorted(folder_list)
     return folder_list
 
 
-data_root = "/home/haimingzhang/Research/Programming/cv-fighter/HDTF_Dataset/HDTF_preprocessed"
+def get_inconsitent_files(root_dir):
+    data_root = "/home/haimingzhang/Research/Programming/cv-fighter/HDTF_Dataset/HDTF_preprocessed"
 
-folder_list = get_folder_list(data_root)
+    folder_list = get_folder_list(data_root)
 
-print(len(folder_list), folder_list[:3])
+    print(len(folder_list), folder_list[:3])
 
-txt = open("bad_data3.txt", 'w')
+    txt = open("bad_data3.txt", 'w')
 
-for folder in tqdm(folder_list):
-    num_face_image = len(os.listdir(osp.join(folder, "face_image")))
-    try:
-        num_mouth_mask = len(os.listdir(osp.join(folder, "mouth_mask")))
-        if num_face_image / 2 != num_mouth_mask:
+    for folder in tqdm(folder_list):
+        num_face_image = len(os.listdir(osp.join(folder, "face_image")))
+        try:
+            num_mouth_mask = len(os.listdir(osp.join(folder, "mouth_mask")))
+            if num_face_image / 2 != num_mouth_mask:
+                txt.write(osp.basename(folder))
+                txt.write("\n")
+        except:
             txt.write(osp.basename(folder))
             txt.write("\n")
-    except:
-        txt.write(osp.basename(folder))
-        txt.write("\n")
-    
-txt.close()
+        
+    txt.close()
+
+
+def count_unprocessed_files():
+    data_root = "/home/haimingzhang/Research/Programming/cv-fighter/HDTF_Dataset/HDTF_preprocessed"
+
+    folder_list = get_folder_list(data_root)
+
+    print(len(folder_list), folder_list[:3])
+
+
+if __name__ == "__main__":
+    count_unprocessed_files()
