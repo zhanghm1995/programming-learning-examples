16cbd5fa3e8af6d8ef29e95c72366b4197060445
Herman <zhanghm_1995@qq.com>
2022-08-21 18:39:34 +0800

    update class_test.py

--- Python_learning/basics/class_test.py
+++ Python_learning/basics/class_test.py
@@ -2,12 +2,10 @@
 ref: https://www.runoob.com/python3/python3-class.html
 """
 
-from turtle import forward
 
-
-class BaseExample:
+class BaseExample(object):
     def __init__(self) -> None:
-        pass
+        print("This is in BaseExample class")
 
     def forward(self):
         print("This is in BaseExample class")
@@ -15,12 +13,34 @@ class BaseExample:
 
 class DerivedExample(BaseExample):
     def __init__(self) -> None:
-        super().__init__()
+        print("============__init__==============")
+        # super(DerivedExample, self).__init__()
+        # super().__init__()
+        super(DerivedExample).__init__()
+        print("This is in DerivedExample class")
     
     def forward(self):
+        print("============forward===============")
         super().forward()
         print("This is in DerivedExample class")
     
+    def area(self):
+        print("compute the area in DerivedExample class")
+
+class DerivedExample2(DerivedExample):
+    def __init__(self) -> None:
+        print("============__init__==============")
+        super(DerivedExample, self).__init__()
+        # super().__init__()
+        print("This is in DerivedExample class")
+
+        super(DerivedExample, self).area()
+    
+    def forward(self):
+        print("============forward===============")
+        super().forward()
+        print("This is in DerivedExample class")
+
 
 if __name__ == "__main__":
     derived_example = DerivedExample()
